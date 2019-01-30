56b66988e9496cfd257476c5aaa406c799305256
jhover <jhover@starkhorn.rhic.bnl.gov>
2019-01-30 10:38:04 -0500

    Update.

---
+++ setup.py
@@ -0,0 +1,43 @@
+#
+# Basic setup file for pip install
+#
+
+import sys
+from setuptools import setup, find_packages
+
+setup(
+    name="pycomanage",
+    version=".99",
+    description='Libraries and utilities for COManage/CILogon usage.',
+    long_description='''Libraries and utilities for COManage/CILogon usage.''',
+    license='BSD',
+    author='John Hover',
+    author_email='jhover@bnl.gov',
+    url='https://github.com/bnl-sdcc/pycomanage',
+    python_requires='>=2.7',
+    packages=find_packages(),
+    install_requires=[],
+    data_files=[
+        # config and cron files
+        ('etc', ['jupyterhub_config_comanage.py',
+                       ]
+         ),
+        # sysconfig
+        #('etc/sysconfig', ['templates/sysconfig/panda_harvester.rpmnew.template',
+        #                   ]
+        # ),
+        # init script
+        #('etc/rc.d/init.d', ['templates/init.d/panda_harvester.rpmnew.template',
+        #                     'templates/init.d/panda_harvester-apachectl.rpmnew.template',
+        #                     'templates/init.d/panda_harvester-uwsgi.rpmnew.template',
+        #                     ]
+        # ),
+        # admin tool
+        ('bin', ['scripts/comanage-gsissh',
+                 ]
+         ),
+        ],
+    scripts=['scripts/comanage-gsissh']
+    )
+
+
